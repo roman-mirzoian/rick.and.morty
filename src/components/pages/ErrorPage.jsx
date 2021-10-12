b02fd393ae97839b832b17957fca0ef56afb9a7d
Roman Mirzoian <roman.mirzoian@playtomax.com>
2021-10-12 03:20:22 +0300

    fix: change error page

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import { Divider } from "@material-ui/core";
 import { Alert } from "@material-ui/core";
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
 
 const ErrorPage = () => {
   return (
     <Divider>
-      <Alert severity="error">Sorry, page not found :(</Alert>
+      <Alert severity="error">Please select the information you are interested in.</Alert><ArrowUpward fontSize="large"/>
     </Divider>
   );
 };
